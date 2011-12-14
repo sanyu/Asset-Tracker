7cc2d09dd636ade7f80433c95fd70db287d9b1ba
Sanyu Melwani <sanyu.md78@gmail.com>
2011-12-14 14:48:38 +0100

    Rails 2.3 adds Object#try, so this extension is no longer needed

--- lib/add_try_to_object.rb
+++
@@ -1,8 +0,0 @@
-#TODO Remove this if we upgrade to Rails 2.3 or greater
-module AddTry
-  def try(method, *args, &block)
-    send(method, *args, &block) if self.class != NilClass and respond_to? method
-  end
-end
-
-Object.send(:include,AddTry)
